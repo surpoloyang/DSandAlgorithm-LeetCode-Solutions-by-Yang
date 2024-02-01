3e1aa4ada349a381087afe8e4aeadac74c274aac
ChenYang <zjuyc101@163.com>
2024-02-01 13:26:16 +0800

    修改“204计数质数”

--- 09Algorithm-Base/枚举算法/204计数质数.py
+++ 09Algorithm-Base/枚举算法/204计数质数.py
@@ -1,13 +1,13 @@
 class Solution:
-    def isPrime(self, x: int) -> bool:
-        for i in range(2, int(pow(x, 0.5))+1):
-            if x % i == 0:
-                return False
-        return True
-    
     def countPrimes(self, n: int) -> int:
-        cnt = 0
-        for i in range(2, n):
-            if self.isPrime(i):
-                cnt += 1
-        return cnt
+        if n <=2 :return 0
+        isPrime = [1] * n
+        isPrime[0] = isPrime[1] = 0 
+        for i in range(2, int(n ** 0.5) + 1):
+            if isPrime[i]:
+                isPrime[i * i:n:i] = [0] * ((n - 1 - i * i) // i + 1)
+
+
+
+        return sum(isPrime)
+
